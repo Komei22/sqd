d621c116351956d72479343ef4ad9513e4f21307
Komei Nomura <komei.nomura@pepabo.com>
2018-10-11 10:50:31 +0900

    lintをかけた

--- matcher/matcher.go
+++ matcher/matcher.go
@@ -37,6 +37,7 @@ func (m *Matcher) loadList(filepath string) error {
 	return m.SaveList(reader)
 }
 
+// SaveList save querys to list
 func (m *Matcher) SaveList(r io.Reader) error {
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
