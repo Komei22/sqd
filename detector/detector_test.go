a577a4ffd88d1f9e72028bcd4cb78344e63d40cb
Komei Nomura <komei.nomura@pepabo.com>
2018-10-12 11:53:45 +0900

    detectorのテストを修正
    標準入出力に対応。エラー判定を修正

--- detector/detector_test.go
+++ detector/detector_test.go
@@ -12,11 +12,13 @@ func TestDumpSuspiciousQuerysUsingBlacklist(t *testing.T) {
 	blacklist := `DROP DATABASE test
 DROP TABLE article`
 
-	querys := `SELECT articles.* FROM articles ORDER BY articles.id DESC LIMIT ?
-DELETE FROM articles WHERE articles.id = ?
-INSERT INTO articles (title, content, created_at, updated_at) VALUES (?, ?, ?, ?)
-DROP DATABASE test
-DROP TABLE article`
+	querys := []string{
+		"SELECT articles.* FROM articles ORDER BY articles.id DESC LIMIT ?",
+		"DELETE FROM articles WHERE articles.id = ?",
+		"INSERT INTO articles (title, content, created_at, updated_at) VALUES (?, ?, ?, ?)",
+		"DROP DATABASE test",
+		"DROP TABLE article",
+	}
 
 	illegalQuerySet := mapset.NewSet(
 		"DROP DATABASE test",
@@ -26,9 +28,7 @@ DROP TABLE article`
 	m := matcher.NewMatcher()
 	m.SaveList(strings.NewReader(blacklist))
 
-	d := newDetector(Blacklist)
-	d.saveQuerys(strings.NewReader(querys))
-
+	d, _ := New(querys, Blacklist)
 	suspiciousQuerys, _ := d.Detect(m)
 
 	suspiciousQuerysSet := mapset.NewSet()
@@ -42,14 +42,16 @@ DROP TABLE article`
 
 func TestDumpSuspiciousQuerysUsingwhitelist(t *testing.T) {
 	whitelist := `SELECT articles.* FROM articles ORDER BY articles.id DESC LIMIT ?
-	DELETE FROM articles WHERE articles.id = ?
-	INSERT INTO articles (title, content, created_at, updated_at) VALUES (?, ?, ?, ?)`
-
-	querys := `SELECT articles.* FROM articles ORDER BY articles.id DESC LIMIT ?
 DELETE FROM articles WHERE articles.id = ?
-INSERT INTO articles (title, content, created_at, updated_at) VALUES (?, ?, ?, ?)
-DROP DATABASE test
-DROP TABLE article`
+INSERT INTO articles (title, content, created_at, updated_at) VALUES (?, ?, ?, ?)`
+
+	querys := []string{
+		"SELECT articles.* FROM articles ORDER BY articles.id DESC LIMIT ?",
+		"DELETE FROM articles WHERE articles.id = ?",
+		"INSERT INTO articles (title, content, created_at, updated_at) VALUES (?, ?, ?, ?)",
+		"DROP DATABASE test",
+		"DROP TABLE article",
+	}
 
 	illegalQuerySet := mapset.NewSet(
 		"DROP DATABASE test",
@@ -59,16 +61,14 @@ DROP TABLE article`
 	m := matcher.NewMatcher()
 	m.SaveList(strings.NewReader(whitelist))
 
-	d := newDetector(Whitelist)
-	d.saveQuerys(strings.NewReader(querys))
-
+	d, _ := New(querys, Whitelist)
 	suspiciousQuerys, _ := d.Detect(m)
 
 	suspiciousQuerysSet := mapset.NewSet()
 	for _, q := range suspiciousQuerys {
 		suspiciousQuerysSet.Add(q)
 	}
-	if suspiciousQuerysSet.Equal(illegalQuerySet) {
+	if !suspiciousQuerysSet.Equal(illegalQuerySet) {
 		t.Error("Failed detect suspicious querys based on whitelist.")
 	}
 }
