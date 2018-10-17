fa637dcbcaee87ec7405db3b54682366c2c70984
Komei Nomura <komei.nomura@pepabo.com>
2018-10-17 12:21:22 +0900

    rootCmdを関数で生成して実行するようにし、エラー出力をreturnするようにした
    テストをやりやすくするために

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
@@ -9,78 +10,77 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-	query     string
-	querylog  string
-	blacklist string
-	whitelist string
-)
+// NewRootCmd return rootCmd
+func newRootCmd() *cobra.Command {
+	var (
+		query     string
+		querylog  string
+		blacklist string
+		whitelist string
+	)
 
-var rootCmd = &cobra.Command{
-	Use:   "sqd",
-	Short: "sqd is suspicious query detection tool based on whitelist or blacklist",
-	Long:  `sqd is suspicious query detection tool based on whitelist or blacklist`,
-	Run: func(cmd *cobra.Command, args []string) {
-		var mode detector.Mode
-		var m *matcher.Matcher
-		var err error
-		if (whitelist == "" && blacklist == "") || (whitelist != "" && blacklist != "") {
-			fmt.Print("Please set list file path, Whitelist(-W) or Blacklist(-B)")
-			os.Exit(1)
-		} else if whitelist == "" && blacklist != "" {
-			mode = detector.Blacklist
-			m, err = matcher.New(blacklist)
-			if err != nil {
-				fmt.Printf("Can't read list file. (%s)", err)
-				os.Exit(1)
+	rootCmd := &cobra.Command{
+		Use:   "sqd",
+		Short: "sqd is suspicious query detection tool based on whitelist or blacklist",
+		Long:  `sqd is suspicious query detection tool based on whitelist or blacklist`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			var mode detector.Mode
+			var m *matcher.Matcher
+			var err error
+			if (whitelist == "" && blacklist == "") || (whitelist != "" && blacklist != "") {
+				return errors.New("Please set list file path, Whitelist(-W) or Blacklist(-B)")
+			} else if whitelist == "" && blacklist != "" {
+				mode = detector.Blacklist
+				m, err = matcher.New(blacklist)
+				if err != nil {
+					return fmt.Errorf("Can't read blacklist file: %s", err)
+				}
+			} else if whitelist != "" && blacklist == "" {
+				mode = detector.Whitelist
+				m, err = matcher.New(whitelist)
+				if err != nil {
+					return fmt.Errorf("Can't read whitelist file: %s", err)
+				}
 			}
-		} else if whitelist != "" && blacklist == "" {
-			mode = detector.Whitelist
-			m, err = matcher.New(whitelist)
-			if err != nil {
-				fmt.Printf("Can't read list file. (%s)", err)
-				os.Exit(1)
+
+			d := detector.New(m, mode)
+
+			var suspiciousQueries []string
+			if querylog != "" {
+				suspiciousQueries, err = d.DetectFrom(querylog)
+				if err != nil {
+					return fmt.Errorf("Can't detection suspicious query: %s", err)
+				}
+			} else {
+				suspiciousQuery, err := d.Detect(query)
+				if err != nil {
+					return fmt.Errorf("Can't detection suspicious query: %s", err)
+				}
+				suspiciousQueries = append(suspiciousQueries, suspiciousQuery)
 			}
-		}
 
-		d := detector.New(m, mode)
-
-		var suspiciousQueries []string
-		if querylog != "" {
-			suspiciousQueries, err = d.DetectFrom(querylog)
-			if err != nil {
-				fmt.Printf("Can't detection suspicious query. (%s)", err)
+			cmd.Println("Suspicious queries:")
+			for _, sq := range suspiciousQueries {
+				cmd.Println(sq)
 			}
-		} else {
-			suspiciousQuery, err := d.Detect(query)
-			if err != nil {
-				fmt.Printf("Can't detection suspicious query. (%s)", err)
-				os.Exit(1)
-			}
-			suspiciousQueries = append(suspiciousQueries, suspiciousQuery)
-		}
 
-		fmt.Print("Suspicious queries:\n")
-		for _, sq := range suspiciousQueries {
-			fmt.Printf("%s\n", sq)
-		}
-	},
-}
-
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
+			return nil
+		},
 	}
-}
-
-func init() {
 	rootCmd.Flags().StringVarP(&query, "query", "q", "", "query string")
 	rootCmd.Flags().StringVarP(&querylog, "file", "f", "", "query log file path")
 	rootCmd.Flags().StringVarP(&whitelist, "Whitelist", "W", "", "whitelist file path")
 	rootCmd.Flags().StringVarP(&blacklist, "Blacklist", "B", "", "blacklist file path")
 
-	cobra.OnInitialize()
+	return rootCmd
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	rootCmd := newRootCmd()
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
 }
