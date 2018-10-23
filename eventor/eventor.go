415a0e83f7d31983676f698676588d4fdf766bfc
Komei Nomura <komei.nomura@pepabo.com>
2018-10-23 14:58:41 +0900

    不要な出力を削除

--- eventor/eventor.go
+++ eventor/eventor.go
@@ -6,7 +6,6 @@ import (
 
 // DumpStdout dump suspicious query for stdout
 func DumpStdout(suspiciousQueries []string) {
-	fmt.Println("Suspicious query:")
 	for _, sq := range suspiciousQueries {
 		fmt.Println(sq)
 	}
