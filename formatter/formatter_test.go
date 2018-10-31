662209ca50b193635b428f5c2de5e32e985ac980
Komei Nomura <komei.nomura@pepabo.com>
2018-10-31 18:18:48 +0900

    formatterのテストを修正

--- formatter/formatter_test.go
+++ formatter/formatter_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestFormat(t *testing.T) {
-	query := `"SELECT * FROM users\nWHERE\n\tname = "test""`
+	query := `SELECT * FROM         users\nWHERE\n\tname = "test"`
 	expect := `SELECT * FROM users WHERE name = "test"`
 
 	formattedQuery := Format(query)
