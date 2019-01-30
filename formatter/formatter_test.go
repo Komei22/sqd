5c0d97893c9775046ce560e03f0ef06db8adabd5
Komei Nomura <komei.nomura@pepabo.com>
2019-01-30 18:02:55 +0900

    formatの処理順を調整

--- formatter/formatter_test.go
+++ formatter/formatter_test.go
@@ -7,10 +7,10 @@ import (
 func TestFormat(t *testing.T) {
 	queries := []string{
 		`SELECT * FROM         users\nWHERE\n\tname = \"test\"`,
-		`SELECT * FROM users WHERE name = \'te\\st\'`}
+		`SELECT * FROM users WHERE name = \'te\\\"st\'`}
 	expects := []string{
 		`SELECT * FROM users WHERE name = "test"`,
-		`SELECT * FROM users WHERE name = 'te\st'`}
+		`SELECT * FROM users WHERE name = 'te\"st'`}
 
 	var formattedQueries []string
 	for _, q := range queries {
