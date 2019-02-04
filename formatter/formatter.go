7df21724166f7b464b4b12f3a5b0c4520308937f
Komei Nomura <komei.nomura@pepabo.com>
2019-02-04 12:34:42 +0900

    入力フォーマットを修正
    入力フォーマットはJSONログではなく，1行に1クエリが記述されている文字列とした．クエリに改行が含まれる場合は，入力する前の段階で取り除いた（sedなどを利用）上で，入力として与えるようにする．

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -1,7 +1,6 @@
 package formatter
 
 import (
-	"fmt"
 	"regexp"
 	"strings"
 )
@@ -10,20 +9,7 @@ var multiSpaceRegexp = regexp.MustCompile(" {2,}")
 
 // Format remove control charactor
 func Format(query string) string {
-	removeStr := []string{"\\n", "\\t"}
-	for _, str := range removeStr {
-		query = strings.Replace(query, str, " ", -1)
-	}
-	query = strings.Replace(query, "\\", "", -1)
+	query = strings.Replace(query, "\t", " ", -1)
 	query = multiSpaceRegexp.ReplaceAllString(query, " ")
 	return query
-}
-
-// ExtractQueryFrom input
-func ExtractQueryFrom(in string) (string, error) {
-	lastIdx := len(in) - 1
-	if string(in[0]) != "\"" || string(in[lastIdx]) != "\"" {
-		return "", fmt.Errorf("Invalid input format: %s", in)
-	}
-	return in[1:lastIdx], nil
 }
