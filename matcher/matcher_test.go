f74b4325977ae23188ff9e106c121e602ddb7b9a
Komei Nomura <komei.nomura@pepabo.com>
2019-06-03 12:36:55 +0900

    fix matcher test
    rewrite table driven test

--- matcher/matcher_test.go
+++ matcher/matcher_test.go
@@ -1,41 +1,83 @@
 package matcher
 
 import (
+	"io"
 	"strings"
 	"testing"
 )
 
-func TestIsMatchListforListingQuery(t *testing.T) {
+func TestMatcher_ReadList(t *testing.T) {
+	type args struct {
+		r io.Reader
+	}
+
 	queryList := `SELECT articles.* FROM articles ORDER BY articles.id DESC LIMIT ?
 DELETE FROM articles WHERE articles.id = ?
 INSERT INTO articles (title, content, created_at, updated_at) VALUES (?, ?, ?, ?)`
+	// legalQueries := []string{
+	// 	"SELECT articles.* FROM articles ORDER BY articles.id DESC LIMIT ?",
+	// 	"DELETE FROM articles WHERE articles.id = ?",
+	// 	"INSERT INTO articles (title, content, created_at, updated_at) VALUES (?, ?, ?, ?)",
+	// }
+	// illegalQuery := `DROP DATABASE production`
 
-	queries := []string{
-		"SELECT articles.* FROM articles ORDER BY articles.id DESC LIMIT ?",
-		"DELETE FROM articles WHERE articles.id = ?",
-		"INSERT INTO articles (title, content, created_at, updated_at) VALUES (?, ?, ?, ?)",
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name:    "Read queries and save mapset.Set",
+			args:    args{r: strings.NewReader(queryList)},
+			wantErr: false,
+		},
 	}
-
-	m := NewMatcher()
-	m.ReadList(strings.NewReader(queryList))
-
-	for _, query := range queries {
-		if !m.IsMatchList(query) {
-			t.Error("Failed distinguish legitimate query.")
-		}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := NewMatcher()
+			if err := m.ReadList(tt.args.r); (err != nil) != tt.wantErr {
+				t.Errorf("Matcher.ReadList() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
 	}
 }
 
-func TestIsMatchListforNotListingQuery(t *testing.T) {
+func TestMatcher_IsMatchList(t *testing.T) {
 	queryList := `SELECT articles.* FROM articles ORDER BY articles.id DESC LIMIT ?
 DELETE FROM articles WHERE articles.id = ?
 INSERT INTO articles (title, content, created_at, updated_at) VALUES (?, ?, ?, ?)`
-	query := "DROP DATABASE production"
-
-	m := NewMatcher()
-	m.ReadList(strings.NewReader(queryList))
-
-	if m.IsMatchList(query) {
-		t.Error("Failed distinguish illegal query.")
+	tests := []struct {
+		name       string
+		argQueries []string
+		want       bool
+	}{
+		{
+			name: "Is match list for listing query",
+			argQueries: []string{
+				"SELECT articles.* FROM articles ORDER BY articles.id DESC LIMIT ?",
+				"DELETE FROM articles WHERE articles.id = ?",
+				"INSERT INTO articles (title, content, created_at, updated_at) VALUES (?, ?, ?, ?)",
+			},
+			want: true,
+		},
+		{
+			name: "Is match list for not listing query",
+			argQueries: []string{
+				"DROP DATABASE production",
+				"DELETE FROM",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := NewMatcher()
+			m.ReadList(strings.NewReader(queryList))
+			for _, arg := range tt.argQueries {
+				if got := m.IsMatchList(arg); got != tt.want {
+					t.Errorf("Matcher.IsMatchList() = %v, want %v", got, tt.want)
+				}
+			}
+		})
 	}
 }
